65a41866b01e60f0af5655ec2391df6ad31d9fa3
Sergio <smyelamos@hiberus.com>
2022-11-05 23:33:41 +0100

    MINOR UPDATE profile.entity #7

--- libs/entity-data-models/src/entities/profile.entity.ts
+++ libs/entity-data-models/src/entities/profile.entity.ts
@@ -1,7 +1,5 @@
+import { Column, Entity } from 'typeorm';
 import { ROLE_ENUM } from '../enums/role.enum';
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray } from 'class-validator';
 import { CrudEntity } from './crud.entity';
 
 /**
@@ -9,8 +7,14 @@ import { CrudEntity } from './crud.entity';
  */
 @Entity()
 export class Profile extends CrudEntity {
-  @ApiProperty({ enum: ROLE_ENUM, enumName: 'ROLE_ENUM' })
+  /* @ApiProperty({ name: 'ROLE_ENUM', enum: ROLE_ENUM })
   @Column('enum')
   @IsArray()
-  roles: ROLE_ENUM[];
+  roles: ROLE_ENUM[]; */
+  @Column({
+    type: 'set',
+    enum: ROLE_ENUM,
+    default: ROLE_ENUM.READER,
+  })
+  role: ROLE_ENUM[];
 }
