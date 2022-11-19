7cca670175f47d797ab6fe2a60777062ceb6d482
Sergio <smyelamos@hiberus.com>
2022-11-19 12:00:28 +0100

    REFACTOR #38

--- libs/ngx-shared/src/ngx-shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ResponseBuilderService } from './lib/services/response-builder.service';
-
-@Module({
-  providers: [ResponseBuilderService],
-  exports: [ResponseBuilderService],
-})
-export class NgxSharedModule {}
