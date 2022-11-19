e0c03eb6ab11e7b9fbc12ca5483e47a461fb9d85
Sergio <smyelamos@hiberus.com>
2022-11-19 12:12:45 +0100

    REFACTO ADD components and modules in backend folder #38

---
+++ backend/src/modules/books/book.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CrudService } from 'src/utilities/services/crud.service';
+import { Repository } from 'typeorm';
+import { BookEntity } from './book.entity';
+
+@Injectable()
+export class BookService extends CrudService<BookEntity> {
+  constructor(
+    @InjectRepository(BookEntity)
+    private readonly booksRepository: Repository<BookEntity>,
+  ) {
+    super(booksRepository);
+  }
+}
