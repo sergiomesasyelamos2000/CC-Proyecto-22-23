e88a2b8633fd4486bef9fd3e83d52567b5f02f33
Sergio <smyelamos@hiberus.com>
2022-11-23 00:16:35 +0100

    MAJOR ADD book-row to book-list #3

--- frontend/src/app/book/book-list/book-row/book-row.component.ts
+++ frontend/src/app/book/book-list/book-row/book-row.component.ts
@@ -1,27 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { GetBookDto } from 'src/app/shared/dtos/book/book-get.dto';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { GetBookDto } from "src/app/shared/dtos/book/book-get.dto";
 
-/** 
+/**
  * Generic component to show book data using a GetBookDto as data source
  * @see {@link book-get.dto}
  */
 
 @Component({
-  selector: 'app-book-row',
-  templateUrl: './book-row.component.html',
-  styleUrls: ['./book-row.component.scss'],
+  selector: "app-book-row",
+  templateUrl: "./book-row.component.html",
+  styleUrls: ["./book-row.component.scss"],
 })
 export class BookRowComponent {
   private _book: GetBookDto = {
-    id:'',
-    ISBN: '',
-    name: '',
-    type: '',
-    description: '',
-    publicationDate: '',
-    editors: '',
-    autors: '',
-    photo: '',
+    id: "",
+    ISBN: "",
+    name: "",
+    type: "",
+    description: "",
+    publicationDate: "",
+    editors: "",
+    autors: "",
+    photo: "",
   };
 
   @Input() set book(book: GetBookDto) {
